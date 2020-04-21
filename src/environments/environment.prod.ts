1ec877f32e1ac2d316403ef407576bacd60eeac1
INIKOLAY <gitcdkey@gmail.com>
2020-04-21 16:58:59 +0500

    update build files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
 };
+
+export const CovidApi = {
+  globalInfo : "https://coronavirus-19-api.herokuapp.com/all",
+  allCountries : "https://coronavirus-19-api.herokuapp.com/countries",
+} 
