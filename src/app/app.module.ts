763970f60230d86a91a294f4ec89de51c3b9b581
INIKOLAY <gitcdkey@gmail.com>
2020-04-21 15:33:39 +0500

    add material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 
 import { HttpClientModule } from '@angular/common/http';
+import { MaterialModule } from 'src/material.module';
 
 @NgModule({
   declarations: [
@@ -18,6 +19,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+
+    //angular material
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
